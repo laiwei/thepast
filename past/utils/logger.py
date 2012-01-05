79e950ec0d35434ac443b5879a491e297f99b55a
laiwei <laiwei@ulaptop.(none)>
2012-01-05 17:03:43 +0800

    add logger.py

---
+++ past/utils/logger.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+
+import logging
+logging.basicConfig(
+        format='%(asctime)s %(levelname)s:%(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.DEBUG)
+
