625a42bb7a0cc79e9eed3ec0a814910d9eeb9b63
laiwei <laiwei.ustc@gmail.com>
2012-02-08 22:28:30 -0500

    use virtualenv in pastme.py

--- pastme.py
+++ pastme.py
@@ -1,5 +1,10 @@
 #-*- coding:utf-8 -*-
 
+import os
+
+activate_this = '%s/env/bin/activate_this.py' % os.path.dirname(os.path.abspath(__file__))
+execfile(activate_this, dict(__file__=activate_this))
+
 from past import app
 #app.run(port=80)
 
