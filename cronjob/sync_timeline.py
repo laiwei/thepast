29d421d7832554c859d4784f4b2ea35271d4ab62
laiwei <work@debian.(none)>
2012-02-14 16:11:56 +0800

    update cron

--- cronjob/sync_timeline.py
+++ cronjob/sync_timeline.py
@@ -1,20 +1,23 @@
 #-*- coding:utf-8 -*-
 
+import sys
+sys.path.append("../")
+
 import os
 import time
 import commands
 
-activate_this = '/home/work/proj/thepast/env/bin/activate_this.py'
+activate_this = '../env/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 
 if __name__ == "__main__":
 
-    print commands.getoutput("python /home/work/proj/thepast/jobs.py -t old -c 101 -n 1")
-    print commands.getoutput("python /home/work/proj/thepast/jobs.py -t old -c 102 -n 1")
-    print commands.getoutput("python /home/work/proj/thepast/jobs.py -t old -c 200 -n 1")
-    print commands.getoutput("python /home/work/proj/thepast/jobs.py -t old -c 400 -n 1")
-    print commands.getoutput("python /home/work/proj/thepast/jobs.py -t new -c 101 -n 1")
-    print commands.getoutput("python /home/work/proj/thepast/jobs.py -t new -c 102 -n 1")
-    print commands.getoutput("python /home/work/proj/thepast/jobs.py -t new -c 200 -n 1")
-    print commands.getoutput("python /home/work/proj/thepast/jobs.py -t new -c 400 -n 1")
+    print commands.getoutput("../env/bin/python /home/work/proj/thepast/jobs.py -t old -c 101 -n 1")
+    print commands.getoutput("../env/bin/python /home/work/proj/thepast/jobs.py -t old -c 102 -n 1")
+    print commands.getoutput("../env/bin/python /home/work/proj/thepast/jobs.py -t old -c 200 -n 1")
+    print commands.getoutput("../env/bin/python /home/work/proj/thepast/jobs.py -t old -c 400 -n 1")
+    print commands.getoutput("../env/bin/python /home/work/proj/thepast/jobs.py -t new -c 101 -n 1")
+    print commands.getoutput("../env/bin/python /home/work/proj/thepast/jobs.py -t new -c 102 -n 1")
+    print commands.getoutput("../env/bin/python /home/work/proj/thepast/jobs.py -t new -c 200 -n 1")
+    print commands.getoutput("../env/bin/python /home/work/proj/thepast/jobs.py -t new -c 400 -n 1")
 
