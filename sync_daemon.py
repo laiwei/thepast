a5eb0f8921c822331e2ab05cf7cfad320b47a7d4
laiwei <work@debian.(none)>
2012-02-14 10:02:27 +0800

    fix demo url, refator sync_daemon

--- sync_daemon.py
+++ sync_daemon.py
@@ -15,28 +15,32 @@ from past import config
 if __name__ == "__main__":
     while True: 
 
-        queue_ids = TaskQueue.get_all_ids()
-        print 'queue length:', len(queue_ids) 
-        for qid in queue_ids:
-            queue = TaskQueue.get(qid)
-            if queue and queue.task_kind == config.K_SYNCTASK:
-                print 'syncing task id:', queue.task_id
-                sync_task = SyncTask.get(queue.task_id)
-                max_sync_times = 0
-                min_id = Status.get_min_origin_id(sync_task.category, sync_task.user_id)
-                if sync_task:
-                    while True:
-                        if max_sync_times >= 3:
-                            break
-                        r = jobs.sync(sync_task, old=True)
-                        new_min_id = Status.get_min_origin_id(sync_task.category, sync_task.user_id)
-                        if r == 0 or new_min_id == min_id:
-                            break
-                        min_id = new_min_id
-                        max_sync_times += 1
-            queue.remove()
+        try:
+            queue_ids = TaskQueue.get_all_ids()
+            print 'queue length:', len(queue_ids) 
+            for qid in queue_ids:
+                queue = TaskQueue.get(qid)
+                if queue and queue.task_kind == config.K_SYNCTASK:
+                    print 'syncing task id:', queue.task_id
+                    sync_task = SyncTask.get(queue.task_id)
+                    max_sync_times = 0
+                    min_id = Status.get_min_origin_id(sync_task.category, sync_task.user_id)
+                    if sync_task:
+                        while True:
+                            if max_sync_times >= 3:
+                                break
+                            r = jobs.sync(sync_task, old=True)
+                            new_min_id = Status.get_min_origin_id(sync_task.category, sync_task.user_id)
+                            if r == 0 or new_min_id == min_id:
+                                break
+                            min_id = new_min_id
+                            max_sync_times += 1
+                queue.remove()
+                time.sleep(5)
+            time.sleep(30)
+        except Exception, e:
+            print '----except: %s' %e
             time.sleep(5)
-        time.sleep(30)
 
         #print commands.getoutput("python jobs.py -t old -c 101 -n 1")
         #print commands.getoutput("python jobs.py -t old -c 102 -n 1")
