cb6f207d8813f53ea5b84fa7a881b13238e05a70
laiwei <work@debian.(none)>
2012-02-28 21:57:53 +0800

    revert logo; update generate pdf

--- cronjob/generate_pdf.py
+++ cronjob/generate_pdf.py
@@ -10,15 +10,24 @@ activate_this = '../env/bin/activate_this.py'
 execfile(activate_this, dict(__file__=activate_this))
 
 from past.utils.pdf import generate_pdf, get_pdf_filename, is_pdf_file_exists
-from past.model.user import User
+from past.model.user import User, UserAlias
+from past import config
 
 if __name__ == "__main__":
     
     for uid in User.get_ids(0, 10000000):
         try:
+            uas = UserAlias.gets_by_user_id(uid)
+            if not uas:
+                continue
+            types = [x.type for x in uas]
+            count = 3000
+            if config.OPENID_TYPE_DICT[config.OPENID_SINA] in types \
+                    or config.OPENID_TYPE_DICT[config.OPENID_QQ] in types:
+                count = 250
             pdf_filename = get_pdf_filename(uid)
             print pdf_filename
-            generate_pdf(pdf_filename, uid, 0, 250, capacity=-1)
+            generate_pdf(pdf_filename, uid, 0, count, capacity=-1)
             if not is_pdf_file_exists(pdf_filename):
                 print '%s generate pdf for user:%s fail' % (datetime.datetime.now(), uid)
             else:
