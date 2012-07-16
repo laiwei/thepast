5f5330bfe19cfa6fd0251d84820090a9eebcba76
laiwei <work@smtp.thepast.me>
2012-07-16 22:16:43 +0800

    找出授权时间超过3个月的用户

--- cronjob/send_refresh_token_mail.py
+++ cronjob/send_refresh_token_mail.py
@@ -16,6 +16,7 @@ from past.store import db_conn
 if __name__ == "__main__":
 
     t = datetime.datetime.now() - datetime.timedelta(days=90)
+    t = t.strftime("%Y-%m-%d %H:%M:%S")
     cursor = db_conn.execute("""select alias_id from oauth2_token where time < %s""", t)
     rows = cursor.fetchall()
     cursor and cursor.close()
@@ -25,12 +26,12 @@ if __name__ == "__main__":
     for row in rows:
         alias_id = row[0]
         c = db_conn.execute("""select `type`, user_id, alias from user_alias where id=%s""", alias_id)
-        r = cursor.fetchone()
+        r = c.fetchone()
         c and c.close()
 
         if r:
             user_and_alias[r[1]].append([r[0], r[2]])
     with open("expire.user", "w") as f:
         for k,v in user_and_alias.items():
-            f.write("%s : %s" %(k,v))
+            f.write("%s : %s\n" %(k,v))
 
