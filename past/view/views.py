9003e8b5991e390891a9b297749c5490467311ea
laiwei <laiwei.ustc@gmail.com>
2012-11-25 23:02:55 +0800

    new css from mumu

--- past/view/views.py
+++ past/view/views.py
@@ -30,48 +30,6 @@ from .utils import require_login, check_access_user, statuses_timelize, get_sync
 
 log = logging.getLogger(__file__)
 
-@app.before_request
-def before_request():
-    g.config = config
-    g.user = auth_user_from_session(session)
-    g.user = User.get(8)
-    if g.user:
-        g.user_alias = UserAlias.gets_by_user_id(g.user.id)
-    else:
-        g.user_alias = None
-
-    if g.user:
-        unbinded = list(set(config.OPENID_TYPE_DICT.values()) - 
-                set([ua.type for ua in g.user.get_alias()]) - set([config.OPENID_TYPE_DICT[config.OPENID_THEPAST]]))
-        tmp = {}
-        for k, v in config.OPENID_TYPE_DICT.items():
-            tmp[v] = k
-        g.unbinded = [[x, tmp[x], config.OPENID_TYPE_NAME_DICT[x]] for x in unbinded]
-
-        expired_providers = []
-        for t in [ua.type for ua in g.user.get_alias()]:
-            p = g.user.get_thirdparty_profile(t)
-            if p and p.get("expired"):
-                _ = [t, config.OPENID_TYPE_DICT_REVERSE.get(t), config.OPENID_TYPE_NAME_DICT.get(t, "")]
-                expired_providers.append(_)
-        if expired_providers:
-            msg = " ".join([x[-1] for x in expired_providers])
-            flash(u"你的 %s 授权已经过期了，会影响数据同步，你可以重新授权 :)" % msg, "tip")
-    else:
-        g.unbinded = None
-
-    g.start = int(request.args.get('start', 0))
-    g.count = int(request.args.get('count', 30))
-    g.cate = request.args.get("cate", "")
-    if g.cate.isdigit():
-        g.cate = int(g.cate)
-    else:
-        g.cate = ""
-        
-@app.teardown_request
-def teardown_request(exception):
-    #http://stackoverflow.com/questions/9318347/why-are-some-mysql-connections-selecting-old-data-the-mysql-database-after-a-del
-    db_conn.commit()
 
 @app.route("/")
 def index():
